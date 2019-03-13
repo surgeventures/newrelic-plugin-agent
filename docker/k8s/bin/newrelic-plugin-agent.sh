56c506b39d80b95f00e408b3ca9d3b6d91a2adfb
michalploski <michal.p@shedul.com>
2019-03-13 16:07:54 +0100

    Ad more variables to configure agent

--- docker/k8s/bin/newrelic-plugin-agent.sh
+++ docker/k8s/bin/newrelic-plugin-agent.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sed -i "s/NEWRELIC_KEY/$NEWRELIC_KEY/g" /etc/newrelic/newrelic-plugin-agent.cfg
-sed -i "s/PGBOUNCER_STATS_USER/$PGBOUNCER_STATS_USER/g" /etc/newrelic/newrelic-plugin-agent.cfg
-sed -i "s/PGBOUNCER_STATS_PASSWORD/$PGBOUNCER_STATS_PASSWORD/g" /etc/newrelic/newrelic-plugin-agent.cfg
-sed -i "s/PGBOUNCER_NAME/$PGBOUNCER_NAME/g" /etc/newrelic/newrelic-plugin-agent.cfg
+
+sed -i "s/NEWRELIC_KEY/${NEWRELIC_KEY}/g" /etc/newrelic/newrelic-plugin-agent.cfg
+sed -i "s/PGBOUNCER_STATS_USER/${PGBOUNCER_STATS_USER}/g" /etc/newrelic/newrelic-plugin-agent.cfg
+sed -i "s/PGBOUNCER_STATS_PASSWORD/${PGBOUNCER_STATS_PASSWORD}/g" /etc/newrelic/newrelic-plugin-agent.cfg
+sed -i "s/PGBOUNCER_NAME/${PGBOUNCER_NAME}/g" /etc/newrelic/newrelic-plugin-agent.cfg
+sed -i "s/PGBOUNCER_HOST/${PGBOUNCER_HOST:=127.0.0.1}/g" /etc/newrelic/newrelic-plugin-agent.cfg
+sed -i "s/PGBOUNCER_PORT/${PGBOUNCER_PORT:=6432}/g" /etc/newrelic/newrelic-plugin-agent.cfg
+sed -i "s/WAKE_INTERVAL/${WAKE_INTERVAL:=30}/g" /etc/newrelic/newrelic-plugin-agent.cfg
 exec newrelic-plugin-agent -f -c /etc/newrelic/newrelic-plugin-agent.cfg
